30d80a5431906d23dd62e606a9745d2f24b170b6
Leonardo Martin <leonardomartin1312@gmail.com>
2022-05-27 20:37:39 -0300

    dark theme toggle

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let toggleTheme = document.querySelector(".toggleTheme")
-let html = document.querySelector(html)
+let html = document.querySelector("html")
 toggleTheme.addEventListener('click', function(){
-  
+  html.classList.toggle("dark-mode")
 })
