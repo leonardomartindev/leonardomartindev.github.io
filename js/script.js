7db1230a6bb81bf7b7b3b467f476be0a0d87917f
Leonardo Martin <leonardomartin1312@gmail.com>
2022-05-31 16:37:13 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -3,21 +3,18 @@ import Countdown from "./countdown.js";
 const btn = document.querySelector("button")
 const data = document.querySelectorAll(".data-time")
 
-
   btn.addEventListener("click", function(){
     const dateInput = document.querySelector(".dateInput").value
     const dateInputFormatado = `"${dateInput}"`
-
+    
     const dataFormatada = new Date(dateInputFormatado)
     const future = new Countdown(dataFormatada)
     
-
-    let interval = setInterval(function(){
-      interval
-        data.forEach((time, index) => {
-          time.innerHTML = future.total[index];    
-        });
-      }, 1000);
-      
-  })
-
+    let intervalo = setInterval(showTime, 1000); 
+    function showTime(){
+      data.forEach((time, index) => {
+        time.innerHTML = future.total[index]; 
+      });    
+  }
+  showTime()
+})
