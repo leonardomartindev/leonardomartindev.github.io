eb524605a1643818f323fb5abef4b1f1c23edd3d
Leonardo Martin <leonardomartin1312@gmail.com>
2022-06-01 17:10:04 -0300

    adicionando função de limpar e blqueando input

--- js/script.js
+++ js/script.js
@@ -1,45 +1,45 @@
 import Countdown from "./countdown.js";
 
-const criaTimer = document.querySelector(".saveDate")
-const limpa = document.querySelector(".limpa")
-const data = document.querySelectorAll(".data-time")
+const criaTimer = document.querySelector(".saveDate");
+const limpa = document.querySelector(".limpa");
+const data = document.querySelectorAll(".data-time");
+const input = document.querySelector(".dateInput");
 
+console.log(data)
 
+criaTimer.addEventListener("click", function(){
+  input.disabled = true
 
-  criaTimer.addEventListener("click", function(){
-    let dateInput = document.querySelector(".dateInput").value
-    const dateInputFormatado = `"${dateInput}"`
-    
-    const dataFormatada = new Date(dateInputFormatado)
-    const future = new Countdown(dataFormatada)
-    
-    let intervalo = setInterval(showTime, 1000); 
-    function showTime(){
-      
-      data.forEach((time, index) => {
-        time.classList.add("cont")
-        time.innerHTML = future.total[index]; 
-      });    
+  let dateInput = document.querySelector(".dateInput").value;
+  const dateInputFormatado = `"${dateInput}"`;
+  const dataFormatada = new Date(dateInputFormatado);
+  const future = new Countdown(dataFormatada);
+  
+  if(input.value === "") {
+    input.disabled = false
+    return
   }
 
-    data.forEach(timer =>{
-      if(timer.classList.contains("cont")){
-        clearInterval(intervalo)
-      }
-    })
+  function showTime(){
+    data.forEach((time, index) => {
+      time.classList.add("cont");
+      time.innerHTML = future.total[index]; 
+    });    
+  }
+  
+  let intervalo = setInterval(showTime, 1000); 
 
-  showTime()
-
-
+  showTime();
 
   limpa.addEventListener("click", function(){
-    clearInterval(intervalo)
+    input.disabled = false
+    clearInterval(intervalo);
     data.forEach(timer => {
-      document.querySelector(".dateInput").value = ""
-      timer.innerHTML = "00"
-      timer.classList.remove("cont")
-    })
-  })
+      document.querySelector(".dateInput").value = "";
+      timer.innerHTML = "00";
+      timer.classList.remove("cont");
+    });
+  });
 
-})
+});
 
