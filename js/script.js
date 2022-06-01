3981b6d67fafc36627a8364d9c0e2165a6b3ece1
Leonardo Martin <leonardomartin1312@gmail.com>
2022-05-31 21:05:25 -0300

    adicionando botão de limpar timer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 import Countdown from "./countdown.js";
 
-const btn = document.querySelector("button")
+const criaTimer = document.querySelector(".saveDate")
+const limpa = document.querySelector(".limpa")
 const data = document.querySelectorAll(".data-time")
 
-  btn.addEventListener("click", function(){
-    const dateInput = document.querySelector(".dateInput").value
+
+
+  criaTimer.addEventListener("click", function(){
+    let dateInput = document.querySelector(".dateInput").value
     const dateInputFormatado = `"${dateInput}"`
     
     const dataFormatada = new Date(dateInputFormatado)
@@ -12,9 +15,31 @@ const data = document.querySelectorAll(".data-time")
     
     let intervalo = setInterval(showTime, 1000); 
     function showTime(){
+      
       data.forEach((time, index) => {
+        time.classList.add("cont")
         time.innerHTML = future.total[index]; 
       });    
   }
+
+    data.forEach(timer =>{
+      if(timer.classList.contains("cont")){
+        clearInterval(intervalo)
+      }
+    })
+
   showTime()
+
+
+
+  limpa.addEventListener("click", function(){
+    clearInterval(intervalo)
+    data.forEach(timer => {
+      document.querySelector(".dateInput").value = ""
+      timer.innerHTML = "00"
+      timer.classList.remove("cont")
+    })
+  })
+
 })
+
